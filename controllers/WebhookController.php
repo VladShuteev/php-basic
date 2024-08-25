1a2375f930a502de7fee7404c761bdb727f46b59
Shuteev Vlad <vlad210495@yandex.ru>
2024-08-25 22:43:01 +0200

    Added process method in Webhook class

---
+++ controllers/WebhookController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class WebhookController extends Controller
+{
+    public function actionProcess()
+    {
+        return "IGQWROQy0xbDlLYjRTR1UwTVhaSmRTYVNxM3JpVWh6cGt4dWNBb1hDRHd0dXFaY2MxWEVkVzJtNkFGYmFrYUE4bTRwVmV0cThYeFJVVzgwZAEwwRGpDa2ZABZAGhPUFptTmdiZAW4wNFRDS1ZAyVklCaWhfalREN1dReEkZD";
+    }
+
+}
